e1f2726e421e223a57470aa0e3cd8b5bdca82cf8
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-16 01:09:10 +0530

    Added a feature to show new item upon clicking on plus icon

--- Watch_out WatchKit Extension/ContentView.swift
+++ Watch_out WatchKit Extension/ContentView.swift
@@ -8,21 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isTextFieldVisible = false
+    @State private var name: String = ""
     var body: some View {
-        ZStack{
+        
+        VStack{
+            
+            HStack{
+                Button(action: {
+                    isTextFieldVisible.toggle()
+                }){
+                    Image(systemName: "plus")
+                    
+                }
+            }.frame(width:100, height:20, alignment:.center)
+           Spacer()
             VStack{
-            
+                
                 ScrollView{
+                    
+                    
+                   
+                       
+                    
+                    
+                    
             VStack(spacing:10){
-            
+                
+                ZStack{
             VStack{
                 
                 
             }.frame(width:200, height:50)
                     .overlay(RoundedRectangle(cornerRadius:20)
-                                    .fill(Color.gray))      //no.1
-                
-                
+                                    .fill(Color.gray))
+                //no.1
+                    if isTextFieldVisible{
+                    VStack{
+                        TextField("Enter Your task here ! ",text:$name)
+                    }
+                    }
+                }//First card(ZSTACk)
+                ZStack{
                 VStack{
                     
                     
@@ -30,6 +57,15 @@ struct ContentView: View {
                     .overlay(RoundedRectangle(cornerRadius:20)
                                     .fill(Color.gray))
                         //no.1
+                    
+                    VStack{
+                         
+                        
+                        
+                        Text("\(name)")
+                    }.frame(width: 100, height: 48)
+                    
+                }//Second card (ZStack)
                 
                 VStack{
                    
@@ -66,17 +102,34 @@ struct ContentView: View {
                     }.frame(width:200, height:50)
                     .overlay(RoundedRectangle(cornerRadius:20)
                                     .fill(Color.gray))
+                
+               
+            
             
             }.frame(maxHeight:.infinity,alignment:.topLeading)
+                            .navigationBarHidden(true)
+                        
             //vstack
-                }.frame(height:.infinity)//for scrollview
-        }
+                    
+                }.frame(height:.infinity)
+                        
+                        
+                   //for scrollview
+                    
+               
+            }
+           
         }//main zstack
+       
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
