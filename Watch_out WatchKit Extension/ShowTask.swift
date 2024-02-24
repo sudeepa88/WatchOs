de5f7c417b0280fcd7b50f7d41181d00aff9399e
sudeepa88 <palsudeepa@gmail.com>
2024-02-24 23:48:50 +0530

    Added Show Task

---
+++ Watch_out WatchKit Extension/ShowTask.swift
@@ -0,0 +1,18 @@
+//
+//  ShowTask.swift
+//  Watch_out WatchKit Extension
+//
+//  Created by Sudeepa Pal on 22/02/24.
+//
+
+import SwiftUI
+
+struct ShowTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowTask()
+}
